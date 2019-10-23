e961fe10b0d63f6302f7fb48f4f2e75f186f0c7b
NeverBreath <492713927@qq.com>
2019-10-23 23:03:38 +0800

    'rabbitMQ'

--- tools/rabbits_MQ.py
+++ tools/rabbits_MQ.py
@@ -1,32 +1,38 @@
 import pika,sys
 
-def fa(mes):
+def fa():
     # 开启socket
     credentials = pika.PlainCredentials('admin', 'K^u2oFI@8Hv*')
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='129.211.17.165', credentials=credentials))
     channel = connection.channel()
     channel.queue_declare(queue='buff_163')  # 声明队列以向其发送消息消息
-    channel.basic_publish(exchange='', routing_key='test',body=mes)  # 注意当未定义exchange时，routing_key需和queue的值保持一致
-    print('send success msg to rabbitmq')
-    connection.close()  # 关闭连接
+    while True:
+        mes = input('请输入消息')
+        if mes:
+            if mes == 'quit':
+                connection.close()  # 关闭连接
+            else:
+                channel.basic_publish(exchange='', routing_key='test',body=mes)  # 注意当未定义exchange时，routing_key需和queue的值保持一致
+                print('send success msg to rabbitmq')
 
 def shou():
     # 建立socket
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='129.211.17.165'))
+    print('开始接受')
+    credentials = pika.PlainCredentials('admin', 'K^u2oFI@8Hv*')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='129.211.17.165', credentials=credentials))
     channel = connection.channel()
-    channel.queue_declare(queue='test')
+    channel.queue_declare(queue='buff_163')
+    print('链接成功')
     def callback(ch, method, properties, body):
         '''回调函数,处理从rabbitmq中取出的消息'''
-        if body:
-            body = 'quit'
-        print("吴伟夫: %s" % body.decode())
-        connection.close()
-
+        mess = body.decode()
+        print("吴伟夫: %s" % mess)
+        if mess and mess == 'quit':
+            connection.close()
     channel.basic_consume('test', callback, auto_ack=True)
     channel.start_consuming()  # 开始监听 接受消息
 
 
 if __name__ == "__main__":
-    mess = input('请输入消息')
-    fa(mess)
-    # shou()
+    # fa()
+    shou()
